36726215b9178dec3701a7a4945c7a7363259b1b
safarsafarov <safarov.safar@hotmail.com>
2020-11-18 16:10:41 +0800

    commit ✨

---
+++ SwiftUi-Views/SwiftUi-Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi-Views
+//
+//  Created by Safar Safarov on 2020/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUi-Views/SwiftUi-Views/SwiftUi_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_ViewsApp.swift
+//  SwiftUi-Views
+//
+//  Created by Safar Safarov on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
