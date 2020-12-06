74b06c59107f76f1f98d0730d7dbd1973c659545
safarsafarov <safarov.safar@hotmail.com>
2020-12-07 03:04:40 +0800

    commit

---
+++ Bookie-SwiftUI/Bookie-SwiftUI/Bookie_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bookie_SwiftUIApp.swift
+//  Bookie-SwiftUI
+//
+//  Created by Safar Safarov on 2020/12/8.
+//
+
+import SwiftUI
+
+@main
+struct Bookie_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
