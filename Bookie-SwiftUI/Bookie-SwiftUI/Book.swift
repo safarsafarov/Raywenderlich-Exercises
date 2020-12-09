1286941cdd19fede9d09d6f4206869acf3728e63
safarsafarov <safarov.safar@hotmail.com>
2020-12-10 02:13:55 +0800

    commit ✨

---
+++ Bookie-SwiftUI/Bookie-SwiftUI/Book.swift
@@ -0,0 +1,12 @@
+//
+//  Book.swift
+//  Bookie-SwiftUI
+//
+//  Created by Safar Safarov on 2020/12/9.
+//
+
+struct Book {
+    let title: String
+    let author: String
+    let image: String 
+}
