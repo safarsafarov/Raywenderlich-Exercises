9a167f0aed24347d3b84fa446c0c7f9c43909bac
safarsafarov <safarov.safar@hotmail.com>
2020-12-10 03:46:11 +0800

    commit

--- Bookie-SwiftUI/Bookie-SwiftUI/Book.swift
+++ Bookie-SwiftUI/Bookie-SwiftUI/Book.swift
@@ -8,5 +8,9 @@
 struct Book {
     let title: String
     let author: String
-    let image: String 
+    
+    init(title: String = "Title", author: String = "Author") {
+        self.title = title
+        self.author = author
+    }
 }
---
+++ Bookie-SwiftUI/Bookie-SwiftUI/Views/BookViews.swift
@@ -0,0 +1,20 @@
+//
+//  BookViews.swift
+//  Bookie-SwiftUI
+//
+//  Created by Safar Safarov on 2020/12/10.
+//
+
+import SwiftUI
+
+struct BookViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookViews_Previews: PreviewProvider {
+    static var previews: some View {
+        BookViews()
+    }
+}
