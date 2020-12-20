63a396354669c4b69db7de2c8bc957b8b7583ab8
safarsafarov <safarov.safar@hotmail.com>
2020-12-20 21:44:43 +0800

    commit ✨

--- Bookie-SwiftUI/Bookie-SwiftUI/Views/ContentView.swift
+++ Bookie-SwiftUI/Bookie-SwiftUI/Views/ContentView.swift
@@ -2,8 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List(Library().sortedBooks, id: \.title) { book in
-            BookRow(book: book)
+        NavigationView{
+            List(Library().sortedBooks, id: \.title) { book in
+                BookRow(book: book)
+            }
         }
     }
 }
