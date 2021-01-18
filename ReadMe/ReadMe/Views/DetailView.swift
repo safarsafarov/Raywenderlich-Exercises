ee7e462df4e7834fc6e92a86abaf373bc02dc4a1
safarsafarov <safarov.safar@hotmail.com>
2021-01-19 01:36:04 +0800

    commit ✨

--- ReadMe/ReadMe/Views/DetailView.swift
+++ ReadMe/ReadMe/Views/DetailView.swift
@@ -4,65 +4,68 @@ import class PhotosUI.PHPickerViewController
 import SwiftUI
 
 struct DetailView: View {
-  let book: Book
-  @Binding var image: UIImage?
-  @State var showingImagePicker = false
-  @State var showingAlert = false
-
-  var body: some View {
-    VStack(alignment: .leading) {
-      TitleAndAuthorStack(
-        book: book,
-        titleFont: .title,
-        authorFont: .title2
-      )
-
-      VStack {
-        Book.Image(
-          uiImage: image,
-          title: book.title,
-          cornerRadius: 16
-        )
-        .scaledToFit()
-
-        HStack {
-            Spacer()
+    let book: Book
+    @Binding var image: UIImage?
+    @State var showingImagePicker = false
+    @State var showingAlert = false
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            TitleAndAuthorStack(
+                book: book,
+                titleFont: .title,
+                authorFont: .title2
+            )
             
-            Button("Delete Image…") {
-              showingAlert = true
+            VStack {
+                Book.Image(
+                    uiImage: image,
+                    title: book.title,
+                    cornerRadius: 16
+                )
+                .scaledToFit()
+                if image != nil {
+                    
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button("Delete Image…") {
+                            showingAlert = true
+                        }
+                        
+                        Spacer()
+                        
+                        Button("Update Image…") {
+                            showingImagePicker = true
+                        }
+                        
+                        .padding()
+                        
+                        Spacer()
+                    }
+                }
             }
-            
-            Spacer()
-            
-            Button("Update Image…") {
-              showingImagePicker = true
-            }
-            
-            .padding()
             
             Spacer()
         }
-      }
-
-      Spacer()
+        .padding()
+        .sheet(isPresented: $showingImagePicker) {
+            PHPickerViewController.View(image: $image)
+        }
+        .alert(isPresented: $showingAlert) {
+            .init(title: .init("Delete image for \(book.title)?"),
+                  primaryButton: .destructive(.init("Delete")) {
+                    image = nil
+                  },
+                  secondaryButton: .cancel())
+        }
     }
-    .padding()
-    .sheet(isPresented: $showingImagePicker) {
-      PHPickerViewController.View(image: $image)
-    }
-    .alert(isPresented: $showingAlert) {
-        .init(title: .init("Delete image for \(book.title)?"),
-              primaryButton: .destructive(.init("Delete")) {
-                image = nil
-              },
-              secondaryButton: .cancel())
-    }
-  }
 }
 
 struct DetailView_Previews: PreviewProvider {
-  static var previews: some View {
-    DetailView(book: .init(), image: .constant(nil))
-      .previewedInAllColorSchemes
-  }
+    static var previews: some View {
+        DetailView(book: .init(), image: .constant(nil))
+            .previewedInAllColorSchemes
+    }
 }
