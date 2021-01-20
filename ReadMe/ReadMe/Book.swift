44a221eed652865baf0baf965dd479392f9a0a2c
safarsafarov <safarov.safar@hotmail.com>
2021-01-21 01:59:57 +0800

    commit ✨

--- ReadMe/ReadMe/Book.swift
+++ ReadMe/ReadMe/Book.swift
@@ -1,14 +1,17 @@
-
 struct Book: Hashable {
-  let title: String
-  let author: String
+    let title: String
+    let author: String
     var microReview: String
-
-  init(
-    title: String = "Title",
-    author: String = "Author"
-  ) {
-    self.title = title
-    self.author = author
-  }
+    var readMe: Bool
+    init(
+        title: String = "Title",
+        author: String = "Author",
+        microReview: String = ""
+        readMe: Bool = true
+    ) {
+        self.title = title
+        self.author = author
+        self.microReview = microReview
+        self.readMe = readMe
+    }
 }
