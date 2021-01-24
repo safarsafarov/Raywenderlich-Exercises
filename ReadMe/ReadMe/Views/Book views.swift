3646f27db63dc5b5a58e3877ee753df9ab4600de
safarsafarov <safarov.safar@hotmail.com>
2021-01-24 12:29:44 +0800

    commit

--- ReadMe/ReadMe/Views/Book views.swift
+++ ReadMe/ReadMe/Views/Book views.swift
@@ -19,98 +19,100 @@ struct BookMarkButton: View {
 
 
 struct TitleAndAuthorStack: View {
-  let book: Book
-  let titleFont: Font
-  let authorFont: Font
-  
-  var body: some View {
-    VStack(alignment: .leading) {
-      Text(book.title)
-        .font(titleFont)
-      Text(book.author)
-        .font(authorFont)
-        .foregroundColor(.secondary)
+    let book: Book
+    let titleFont: Font
+    let authorFont: Font
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(book.title)
+                .font(titleFont)
+            Text(book.author)
+                .font(authorFont)
+                .foregroundColor(.secondary)
+        }
     }
-  }
 }
 
 extension Book {
-  struct Image: View {
-    let uiImage: UIImage?
-    let title: String
-    var size: CGFloat?
-    let cornerRadius: CGFloat
-
-    var body: some View {
-      if let image = uiImage.map(SwiftUI.Image.init) {
-        image
-          .resizable()
-          .scaledToFill()
-          .frame(width: size, height: size)
-          .cornerRadius(cornerRadius)
-      } else {
-        let symbol =
-          SwiftUI.Image(title: title)
-          ?? .init(systemName: "book")
-
-        symbol
-          .resizable()
-          .scaledToFit()
-          .frame(width: size, height: size)
-          .font(Font.title.weight(.light))
-          .foregroundColor(.secondary)
-      }
+    struct Image: View {
+        let uiImage: UIImage?
+        let title: String
+        var size: CGFloat?
+        let cornerRadius: CGFloat
+        
+        var body: some View {
+            if let image = uiImage.map(SwiftUI.Image.init) {
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size, height: size)
+                    .cornerRadius(cornerRadius)
+            } else {
+                let symbol =
+                    SwiftUI.Image(title: title)
+                    ?? .init(systemName: "book")
+                
+                symbol
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: size, height: size)
+                    .font(Font.title.weight(.light))
+                    .foregroundColor(.secondary)
+            }
+        }
     }
-  }
 }
 
 struct Book_Previews: PreviewProvider {
-  static var previews: some View {
-    VStack {
-      TitleAndAuthorStack(
-        book: .init(),
-        titleFont: .title,
-        authorFont: .title2
-      )
-      Book.Image(title: Book().title)
-      Book.Image(title: "")
-      Book.Image(title: "📖")
+    static var previews: some View {
+        HStack{
+            VStack {
+                TitleAndAuthorStack(
+                    book: .init(),
+                    titleFont: .title,
+                    authorFont: .title2
+                )
+                Book.Image(title: Book().title)
+                Book.Image(title: "")
+                Book.Image(title: "📖")
+            }
+        }
+        .previewedInAllColorSchemes
     }
-    .previewedInAllColorSchemes
-  }
 }
 
 extension Image {
-  init?(title: String) {
-    guard
-      let character = title.first,
-      case let symbolName = "\(character.lowercased()).square",
-      UIImage(systemName: symbolName) != nil
-    else {
-      return nil
+    init?(title: String) {
+        guard
+            let character = title.first,
+            case let symbolName = "\(character.lowercased()).square",
+            UIImage(systemName: symbolName) != nil
+        else {
+            return nil
+        }
+        
+        self.init(systemName: symbolName)
     }
-
-    self.init(systemName: symbolName)
-  }
 }
 
 extension Book.Image {
-  /// A preview Image.
-  init(title: String) {
-    self.init(
-      uiImage: nil,
-      title: title,
-      cornerRadius: .init()
-    )
-  }
+    /// A preview Image.
+    init(title: String) {
+        self.init(
+            uiImage: nil,
+            title: title,
+            cornerRadius: .init()
+        )
+    }
 }
 
 
 extension View {
-  var previewedInAllColorSchemes: some View {
-    ForEach(
-      ColorScheme.allCases, id: \.self,
-      content: preferredColorScheme
-    )
-  }
+    var previewedInAllColorSchemes: some View {
+        ForEach(
+            ColorScheme.allCases, id: \.self,
+            content: preferredColorScheme
+        )
+    }
 }
