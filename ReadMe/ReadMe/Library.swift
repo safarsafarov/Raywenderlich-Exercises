323c2238f6be1a3165fe4a78898b0c0a01539faa
safarsafarov <safarov.safar@hotmail.com>
2021-01-31 14:24:26 +0800

    commit ✨

--- ReadMe/ReadMe/Library.swift
+++ ReadMe/ReadMe/Library.swift
@@ -1,15 +1,16 @@
-
+import Combine
 import class UIKit.UIImage
 
-struct Library {
+class Library: ObservableObject {
     var sortedBooks: [Book] { booksCache }
     /// Adds a new book at the start of the library's manually-sorted books.
-    mutating func addNewBook(_ book: Book, image: UIImage?) {
+    func addNewBook(_ book: Book, image: UIImage?) {
         booksCache.insert(book, at: 0)
+        uiImages[book] = image
     }
     
     /// An in-memory cache of the manually-sorted books that are persistently stored.
-    private var booksCache: [Book] = [
+    @Published private var booksCache: [Book] = [
         .init(title: "Ein Neues Land", author: "Shaun Tan", microReview: "Earthily Delightful"),
         .init(title: "Bosch", author: "Laurinda Dixon", microReview: "Blastatstic"),
         .init(title: "Dare to Lead", author: "Brené Brown", microReview: "Interesting"),
@@ -23,5 +24,5 @@ struct Library {
         .init(title: "What to Say When You Talk to Yourself", author: "Shad Helmstetter")
     ]
     
-    var uiImages: [Book: UIImage] = [:]
+    @Published var uiImages: [Book: UIImage] = [:]
 }
--- ReadMe/ReadMe/Views/NewBookView.swift
+++ ReadMe/ReadMe/Views/NewBookView.swift
@@ -3,14 +3,25 @@ import SwiftUI
 struct NewBookView: View {
     @ObservedObject var book = Book(title: "", author: "")
     @State var image: UIImage? = nil
+    @EnvironmentObject var library: Library
     
     var body: some View {
-        VStack(spacing: 24) {
-            TextField("Title", text: $book.title)
-            TextField("Author", text: $book.author)
-            ReviewAndImageStack(book: book, image: $image)
+        NavigationView{
+            VStack(spacing: 24) {
+                TextField("Title", text: $book.title)
+                TextField("Author", text: $book.author)
+                ReviewAndImageStack(book: book, image: $image)
+            }
+            .padding()
+            .navigationBarTitle("Got a New Book?")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Add to Library") {
+                        
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
